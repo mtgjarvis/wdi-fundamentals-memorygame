c3577764792c5d280605b8abf0ba6800dc974498
Mark Jarvis <markjarvis@Marks-MacBook-Pro.local>
2019-06-20 20:05:10 -0300

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,23 @@
 console.log("Up and running!");
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+
+var cardOne = cards[0];
+var cardTwo = cards[2];
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+if (cardsInPlay[0] === cardsInPlay[1]) {
+	alert("you found a match!");
+} else {
+	alert("Sorry try again.");
+}
+
+
+
+
 
 console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+console.log("User flipped " + cardTwo);
